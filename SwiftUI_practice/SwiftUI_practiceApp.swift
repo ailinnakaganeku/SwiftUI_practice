5188ee027ac8a4dc0b318379a339c620b313a9bf
ailin.nakaganeku <ailin.nakaganeku@zenvia.com>
2023-03-28 01:20:47 -0300

    Initial Commit

---
+++ SwiftUI_practice/SwiftUI_practiceApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_practiceApp.swift
+//  SwiftUI_practice
+//
+//  Created by Ailin Nakaganeku on 28/03/2023.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_practiceApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
